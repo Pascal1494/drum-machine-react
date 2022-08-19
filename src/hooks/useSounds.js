0a8eb8c08d89208c9f352c85791352e0153f4cd5
Pascal1494 <discokilly@laposte.net>
2022-08-19 23:09:58 +0200

    songs on keybord touch

--- src/hooks/useSounds.js
+++ src/hooks/useSounds.js
@@ -12,11 +12,11 @@ export default function useSounds() {
     const mySampler = useRef(null);
     useEffect(() => {
         const sampler = new Tone.Sampler({
-                "C4": casta,
-                "D#4": clap,
-                "F#4": hh,
-                "A4": kick,
-           
+            "C4": casta,
+            "D#4": clap,
+            "F#4": hh,
+            "A4": kick,
+
         }).toDestination();
 
 
@@ -26,19 +26,51 @@ export default function useSounds() {
 
     }, []);
 
+    function soundPlay(note) {
+        mySampler.current.triggerAttackRelease([note], 4);
+    }
+
+    function handleKeyDown({ key }) {
+        switch (key) {
+            case "a":
+                soundPlay("C4");
+                break;
+           
+            case "z":
+                soundPlay("D#4");
+                break;
+            
+            case "e":
+                soundPlay("F#4");
+                break;
+            
+            case "r":
+                soundPlay("A4");
+                break;
+            default:
+    }
+    }
+
+    useEffect(() => {
+        window.addEventListener("keydown", handleKeyDown);
+        return () => {
+            window.removeEventListener("keydown", handleKeyDown);
+        };
+    }, [])
+
     const buttonsList = [
-        { 
-            soundPlay: () => mySampler.current.triggerAttackRelease(["C4"], 4),
-    },
-        { 
-            soundPlay: () => mySampler.current.triggerAttackRelease(["D#4"], 4),
-    },
-        { 
-            soundPlay: () => mySampler.current.triggerAttackRelease(["F#4"], 4),
-    },
-        { 
-            soundPlay: () => mySampler.current.triggerAttackRelease(["A4"], 4), 
-    },
+        {
+            soundPlay: () => soundPlay("C4")
+        },
+        {
+            soundPlay: () => soundPlay("D#4")
+        },
+        {
+            soundPlay: () => soundPlay("F#4")
+        },
+        {
+            soundPlay: () => soundPlay("C4")
+        },
     ];
 
 
