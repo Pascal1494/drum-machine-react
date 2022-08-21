eaecb62806d9d09c6a4e9bb9b9c831adb510bb9f
Pascal1494 <discokilly@laposte.net>
2022-08-21 13:04:14 +0200

    last commit 2

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,9 +3,10 @@ import styled from 'styled-components'
 
 
 export default function About() {
-return<Wrapper>
-<h1>About Page </h1>
-</Wrapper>
+    return <Wrapper>
+        <h1>Lien du GitHub </h1>
+        <a href="https://github.com/Pascal1494/drum-machine-react"><img className='logo-git' src="https://icones.pro/wp-content/uploads/2021/06/symbole-github-violet.png" alt="logo" />lien</a>
+    </Wrapper>
 
 }
 const Wrapper = styled.div``;
