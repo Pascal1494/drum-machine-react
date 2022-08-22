ca94b5e1b8e05a9b8b6a041d8edc5b32f6489a5c
Pascal1494 <discokilly@laposte.net>
2022-08-22 08:21:40 +0200

    for build

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,12 +1,36 @@
-import styled from 'styled-components'
-
-
+import styled from "styled-components";
 
 export default function About() {
-    return <Wrapper>
-        <h1>Lien du GitHub </h1>
-        <a href="https://github.com/Pascal1494/drum-machine-react"><img className='logo-git' src="https://icones.pro/wp-content/uploads/2021/06/symbole-github-violet.png" alt="logo" />lien</a>
+  return (
+    <Wrapper>
+      <h1>Lien du GitHub </h1>
+      <a href="https://github.com/Pascal1494/drum-machine-react">
+        <img
+          className="logo-git"
+          src="https://icones.pro/wp-content/uploads/2021/06/symbole-github-violet.png"
+          alt="logo"
+        />
+      </a>
     </Wrapper>
-
+  );
 }
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  width: 100%;
+  ${"" /* display: flex; */}
+  align-items: center;
+
+  h1 {
+    width: 100%;
+    color: lightgrey;
+    font-size: 3rem;
+    text-align: center;
+    margin: 3rem auto;
+    text-transform: uppercase;
+  }
+
+  .logo-git {
+    display: flex;
+    margin:  auto;
+    width: 160px;
+  }
+`;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,55 +1,63 @@
-import styled from 'styled-components';
-import { Link, useLocation } from 'react-router-dom'
-
-
+import styled from "styled-components";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Header() {
-    const location = useLocation()
-   
-    return <Wrapper>
-        <h1>Drum-Machine </h1>
-        <nav>
-            <Link to="/">
-                <MenuEl isCurrentPage={location.pathname === "/"}>Home</MenuEl>
-            </Link>
-            <Link to="/about">
-                <MenuEl isCurrentPage={location.pathname === "/about"}>About</MenuEl>
-            </Link>
-        </nav>
-       
+  const location = useLocation();
+
+  return (
+    <Wrapper>
+      <h1>Drum-Machine </h1>
+      <a href="/">
+        <img
+          className="drum"
+          src="https://i.pinimg.com/564x/5f/85/0a/5f850a0e7bcae113ad164462dffe6294.jpg"
+          alt=""
+          srcset=""
+        />
+      </a>
+      <nav>
+        <Link to="/">
+          <MenuEl isCurrentPage={location.pathname === "/"}>Home</MenuEl>
+        </Link>
+        <Link to="/about">
+          <MenuEl isCurrentPage={location.pathname === "/about"}>About</MenuEl>
+        </Link>
+      </nav>
     </Wrapper>
-};
+  );
+}
 
 const Wrapper = styled.header`
-    height: 80px;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0px 24px;
+  align-items: center;
+  // border-bottom: solid 1px;
+
+  & a {
+    text-decoration: none;
+    color: inherit;
+  }
+  & a:first-child {
+    margin-right: 16px;
+  }
+  nav {
     display: flex;
-    justify-content: space-between;
-    padding: 0px 24px;
-    align-items: center;
-    // border-bottom: solid 1px;
-
-    & a {
-        text-decoration: none;
-        color: inherit;
-
-    }
-    & a:first-child {
-        margin-right: 16px;
-        
-    }
-    nav {
-        display: flex;
-        
-    }
-
+  }
+  .drum {
+    border-radius: 15px;
+    width: 100px;
+  }
 `;
 
 const MenuEl = styled.p`
-font-size: 18px;
-padding-bottom:2px;
-border-bottom: solid 2px ${props=>props.isCurrentPage ? "": "transparent"};
-&:hover {
+  font-size: 18px;
+  padding-bottom: 2px;
+  border-bottom: solid 2px
+    ${(props) => (props.isCurrentPage ? "" : "transparent")};
+  &:hover {
     transition: linear 0.15s;
-    color:#FA2FB5;
-}
+    color: #fa2fb5;
+  }
 `;
